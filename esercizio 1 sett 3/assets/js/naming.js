b1b9f375af1c6a0251430b7f50198d5da9401f43
lauragrose <114408417+lauragrose@users.noreply.github.com>
2022-10-10 16:11:29 +0200

    Add files via upload

---
+++ esercizio 1 sett 3/assets/js/naming.js
@@ -0,0 +1,27 @@
+document.write('<link rel="stylesheet" type="text/css" href="../css/style.css">')
+
+var pet_preferito = 'gatto';
+console.log(1+pet_preferito);
+
+var pet = 'gatto';
+var Pet = '<p>Il tuo animale preferito è: il cane</p>';
+
+
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+
+console.log($pet);
+
+var _pet = 'leone';
+console.log(_pet);
+
+var _pet2 = 'pantera';
+document.write(Pet);
+
+document.write('<h3>Questa scritta sarà in fondo alla pagina, ciao a tutti.</h3>');
+
+var numero12 = 'numero';
+alert('Sei sicuro di voler visualizzare questa pagina?')
+
